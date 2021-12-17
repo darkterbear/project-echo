318f2a0d8e8bc9b4f4f6105ba9029a70855fb02e
Terrance Li <terranceli.terbear@gmail.com>
2021-12-17 00:12:17 -0800

    Fullscreen threejs canvas with dummy models

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
+import './css/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import GamePage from './GamePage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GamePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
