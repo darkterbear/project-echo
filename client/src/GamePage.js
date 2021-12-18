be274abb85882b67cf5a71ccb80f8e4895ccdff3
Terrance Li <terranceli.terbear@gmail.com>
2021-12-17 20:52:03 -0800

    Add farm building and shadows

--- client/src/GamePage.js
+++ client/src/GamePage.js
@@ -16,19 +16,21 @@ function GamePage() {
         rotation: [Math.PI / 6, 0, 0], 
         position: [0, 0 - (10 / Math.sqrt(3)), 10]
       }}>
-        {/* <CameraMover canvasRef={canvasRef} /> */}
+        <CameraMover canvasRef={canvasRef} speed={0.5} />
         <color attach="background" args={[SKY_BLUE]} />
 
-        <ambientLight intensity={1} />
-        <directionalLight castShadow intensity={2} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
+        <ambientLight intensity={1} castShadow />
+        <directionalLight castShadow intensity={1} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
           <orthographicCamera attach="shadow-camera" left={-20} right={20} top={20} bottom={-20} />
         </directionalLight>
 
         {/* Puts 0, 0 at the center of the bottom left square */}
         <gridHelper args={[GRID_SIZE, GRID_SIZE, GRID_LINE_COLOR, GRID_LINE_COLOR]} rotation={[Math.PI / 2, 0, 0]} position={[GRID_SIZE / 2 - 0.5, GRID_SIZE / 2 - 0.5, 0.01]} />
-        <Plane position={[GRID_SIZE / 2 - 0.5, GRID_SIZE / 2 - 0.5, -PLANE_THICKNESS / 2]} dimensions={[GRID_SIZE, GRID_SIZE, PLANE_THICKNESS]} color={GRASS_GREEN} />
+        <Plane receiveShadow position={[GRID_SIZE / 2 - 0.5, GRID_SIZE / 2 - 0.5, -PLANE_THICKNESS / 2]} dimensions={[GRID_SIZE, GRID_SIZE, PLANE_THICKNESS]} color={GRASS_GREEN} />
 
         <Building position={[1, 1]} type={BuildingType.NEXUS} />
+        <Building position={[4, 1]} type={BuildingType.POWER_PLANT} />
+        <Building position={[1, 5]} type={BuildingType.FARM} />
       </Canvas>
     </React.Fragment>
   );
--- client/src/components/Building.js
+++ client/src/components/Building.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import { PLANE_THICKNESS } from '../util';
 
 export const BuildingType = {
   NEXUS: 'nexus',
@@ -11,20 +10,65 @@ export const BuildingType = {
   WATCHTOWER: 'watchtower',
 };
 
-const BUILDING_HEIGHT = 1.5;
-
+/**
+ * Three.js mesh component for a building.
+ * @param {Number[2]} position: [x, y], describing lower left corner of the building
+ * @param {BuildingType} type: enum describing type of building
+ * @returns 
+ */
 export default function Building({ type, position }) {
   const mesh = useRef()  
 
   switch(type) {
     case BuildingType.NEXUS:
+      const NEXUS_HEIGHT = 1.5;
       return (
-        <mesh position={[...position, BUILDING_HEIGHT / 2]} rotation={[Math.PI / 2, Math.PI / 4, 0]} ref={mesh}>
-          <coneGeometry args={[Math.sqrt(2), BUILDING_HEIGHT, 4]} />
+        <mesh castShadow position={[position[0] + 1, position[1] + 1, NEXUS_HEIGHT / 2]} rotation={[Math.PI / 2, Math.PI / 4, 0]} ref={mesh}>
+          <coneGeometry args={[Math.sqrt(2), NEXUS_HEIGHT, 4]} />
           <meshStandardMaterial color={'white'} />
         </mesh>
       );
-    default:
-      return null;
+      case BuildingType.POWER_PLANT:
+        return (
+          <mesh castShadow position={[position[0] + 0.5, position[1] + 0.5, 0.2]} rotation={[Math.PI / 2, Math.PI / 4, 0]} ref={mesh}>
+            <cylinderGeometry args={[0.5, 0.5, 0.4, 32]} />
+            <meshStandardMaterial color={'white'} />
+          </mesh>
+        );
+      case BuildingType.FARM:
+
+        return (
+          <>
+            {/* Fences */}
+            <mesh castShadow position={[position[0] + 0.05, position[1], 0.2]} ref={mesh}>
+              <boxGeometry args={[0.1, 2, 0.4]} />
+              <meshStandardMaterial color={'white'} />
+            </mesh>
+            <mesh castShadow position={[position[0] + 1.95, position[1], 0.2]} ref={mesh}>
+              <boxGeometry args={[0.1, 2, 0.4]} />
+              <meshStandardMaterial color={'white'} />
+            </mesh>
+            <mesh castShadow position={[position[0] + 1, position[1] - 0.95, 0.2]} ref={mesh}>
+              <boxGeometry args={[2, 0.1, 0.4]} />
+              <meshStandardMaterial color={'white'} />
+            </mesh>
+            <mesh castShadow position={[position[0] + 1, position[1] + 0.95, 0.2]} ref={mesh}>
+              <boxGeometry args={[2, 0.1, 0.4]} />
+              <meshStandardMaterial color={'white'} />
+            </mesh>
+
+            {/* Animals */}
+            <mesh castShadow position={[position[0] + 0.55, position[1] + 0.15, 0.1]} ref={mesh}>
+              <boxGeometry args={[0.3, 0.2, 0.2]} />
+              <meshStandardMaterial color={'white'} />
+            </mesh>
+            <mesh castShadow position={[position[0] + 0.95, position[1] - 0.25, 0.1]} ref={mesh}>
+              <boxGeometry args={[0.2, 0.3, 0.2]} />
+              <meshStandardMaterial color={'white'} />
+            </mesh>
+          </>
+        );
+      default:
+        return null;
   }
 }
